1d7b5e679e42e24332eee0f28e80ecc0a18c5862
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-05 08:44:32 -0400

    Scoreboard Working Properly, now onto CSS.

--- src/components/ScoreCounter.vue
+++ src/components/ScoreCounter.vue
@@ -1,27 +1,46 @@
 <template>
   <div>
-    <h3>Team</h3>
-    <p>0</p>
+    <h3>{{teamDisplayName}}</h3>
+    <p>{{teamDisplayScore}}</p>
     <div>
-      <input placeholder="Update Team Name" type="text" name="" value="">
-      <button type="Update" name="button" v-on:click="updateName">Update</button>
+      <input placeholder="Update Team Name" id="teamInput" type="text" v-model="teamName">
+      <button type="Update" name="button" id="updateNameButton" v-on:click="updateName">Update</button>
     </div>
     <div class="">
       <span>Update Score</span>
-      <button type="button" name="button" v-on:click="">Add 1</button>
-      <button type="button" name="button" v-on:click="">Subtract 1</button>
+      <button type="button" name="button" v-on:click="addOne">Add 1</button>
+      <button type="button" name="button" v-on:click="subtractOne">Subtract 1</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ScoreCounter',
+  name: "ScoreCounter",
   props: {
     team: String
   },
-
-}
+  data: function() {
+    return {
+      teamName: "",
+      teamDisplayName: "Team",
+      teamDisplayScore: 0,
+    };
+  },
+  methods: {
+    updateName: function() {
+      this.teamDisplayName = this.teamName;
+    },
+    addOne: function() {
+      this.teamDisplayScore++;
+    },
+    subtractOne: function() {
+      if (this.teamDisplayScore > 0) {
+        this.teamDisplayScore--;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="css">
